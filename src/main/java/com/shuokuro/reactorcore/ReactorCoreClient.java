d1fc8abe8d88addc7718e9e632c40b1333446dfe
ShuoKuro <timehet556947@gmail.com>
2025-10-27 14:16:44 +0800

    Getting Started. Basic environment setup completed and The client can be successfully opened

--- src/main/java/com/shuokuro/reactorcore/ReactorCoreClient.java
+++ src/main/java/com/shuokuro/reactorcore/ReactorCoreClient.java
@@ -1,4 +1,4 @@
-package com.example.examplemod;
+package com.shuokuro.reactorcore;
 
 import net.minecraft.client.Minecraft;
 import net.neoforged.api.distmarker.Dist;
@@ -11,11 +11,11 @@ import net.neoforged.neoforge.client.gui.ConfigurationScreen;
 import net.neoforged.neoforge.client.gui.IConfigScreenFactory;
 
 // This class will not load on dedicated servers. Accessing client side code from here is safe.
-@Mod(value = ExampleMod.MODID, dist = Dist.CLIENT)
+@Mod(value = ReactorCore.MOD_ID, dist = Dist.CLIENT)
 // You can use EventBusSubscriber to automatically register all static methods in the class annotated with @SubscribeEvent
-@EventBusSubscriber(modid = ExampleMod.MODID, value = Dist.CLIENT)
-public class ExampleModClient {
-    public ExampleModClient(ModContainer container) {
+@EventBusSubscriber(modid = ReactorCore.MOD_ID, value = Dist.CLIENT)
+public class ReactorCoreClient {
+    public ReactorCoreClient(ModContainer container) {
         // Allows NeoForge to create a config screen for this mod's configs.
         // The config screen is accessed by going to the Mods screen > clicking on your mod > clicking on config.
         // Do not forget to add translations for your config options to the en_us.json file.
@@ -24,8 +24,6 @@ public class ExampleModClient {
 
     @SubscribeEvent
     static void onClientSetup(FMLClientSetupEvent event) {
-        // Some client setup code
-        ExampleMod.LOGGER.info("HELLO FROM CLIENT SETUP");
-        ExampleMod.LOGGER.info("MINECRAFT NAME >> {}", Minecraft.getInstance().getUser().getName());
+        // Client setup code here
     }
 }
